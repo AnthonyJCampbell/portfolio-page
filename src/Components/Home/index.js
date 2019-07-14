ad9aca76a4ed2d9e78e8028b24a6c8acb941248d
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-07-14 09:53:01 +0200

    Scaffolding app

---
+++ src/Components/Home/index.js
@@ -0,0 +1,11 @@
+import Bio from './Bio/Bio';
+import Header from './Header/Header';
+import Project from './ProjectsContainer/Project';
+import ProjectsContainer from './ProjectsContainer/ProjectsContainer';
+
+module.exports = {
+  Bio,
+  Header,
+  Project,
+  ProjectsContainer,
+};
---
+++ src/Components/Projects/index.js
@@ -0,0 +1,9 @@
+import ProjectHeader from './ProjectHeader/ProjectHeader';
+import ProjectFooter from './ProjectFooter/ProjectFooter';
+import ContentContainer from './ContentContainer/ContentContainer';
+
+module.exports = {
+  ProjectFooter,
+  ProjectHeader,
+  ContentContainer,
+};
