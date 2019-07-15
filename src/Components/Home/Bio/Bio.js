3b731e3a7b36feded78a4d79dce55096bb5bc5f5
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-07-15 14:57:12 +0200

    Very basic layout finished

--- src/Components/Home/Bio/Bio.js
+++ src/Components/Home/Bio/Bio.js
@@ -2,7 +2,39 @@ import React from 'react';
 
 const Bio = () => {
   return (
-    <div>Bio</div>
+    <div>
+      <h2>About Me</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Nemo doloremque rerum et eum amet exercitationem, quasi a unde.
+        At placeat exercitationem molestias ea ab praesentium,
+        perspiciatis id reiciendis blanditiis vitae.
+      </p>
+
+      <h3>Skills</h3>
+      {/* Dropdowns that reveal the skills */}
+      <h6>Frameworks</h6>
+      <ul>
+        <li>React</li>
+        <li>Angular 2</li>
+        <li>Vue</li>
+        <li>Express</li>
+        <li>Redux</li>
+      </ul>
+      <h6>Databases</h6>
+      <ul>
+        <li>MongoDB</li>
+        <li>MySQL</li>
+        <li>SQLite3</li>
+      </ul>
+
+      <nav>
+        <div>Resume</div>
+        <div>GitHub</div>
+        <div>Twitter</div>
+        <div>Email</div>
+      </nav>
+    </div>
   );
 };
 
